ceb33fe3f26c139ae85477c88716782e8171e80f
Lucy Mielke <mielkesteven@icloud.com>
2022-07-31 20:20:18 +0200

    modified script

--- 1_build-only-sl.sh
+++ 1_build-only-sl.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 
 
+
 echo "===================================================================";
-echo "setting variables, creating environment...";
+echo "Setting environment vars....";
 echo "===================================================================";
 set +h;
 umask 022;
 export SL=~/Dokumente/suckless-linux/suckless;
-mkdir -pv ${SL};
 export LC_ALL=POSIX;
 export PATH=${SL}/cross-tools/bin:/bin:/usr/bin;
+unset CFLAGS;
+unset CXXFLAGS;
+export SL_HOST=$(echo ${MACHTYPE} | sed "s/-[^-]*/-cross/");
+export SL_TARGET=x86_64-unknown-linux-gnu;
+export SL_CPU=k8;
+export SL_ARCH=$(echo ${SL_TARGET} | sed -e 's/-.*//' -e 's/i.86/i386/');
+export SL_ENDIAN=little;
+export CC="${SL_TARGET}-gcc";
+export CXX="${SL_TARGET}-g++";
+export CPP="${SL_TARGET}-gcc -E";
+export AR="${SL_TARGET}-ar";
+export AS="${SL_TARGET}-as";
+export LD="${SL_TARGET}-ld";
+export RANLIB="${SL_TARGET}-ranlib";
+export READELF="${SL_TARGET}-readelf";
+export STRIP="${SL_TARGET}-strip";
 
 
 echo "===================================================================";
@@ -223,36 +239,7 @@ echo "===================================================================";
 touch ${SL}/var/run/utmp ${SL}/var/log/{btmp,lastlog,wtmp};
 chmod -v 664 ${SL}/var/run/utmp ${SL}/var/log/lastlog;
 
-
-echo "===================================================================";
-echo "Setting environment vars....";
-echo "===================================================================";
-
-set +h;
-umask 022;
-export SL=~/Dokumente/suckless-linux/suckless;
-export LC_ALL=POSIX;
-export PATH=${SL}/cross-tools/bin:/bin:/usr/bin;
-unset CFLAGS;
-unset CXXFLAGS;
-export SL_HOST=$(echo ${MACHTYPE} | sed "s/-[^-]*/-cross/");
-export SL_TARGET=x86_64-unknown-linux-gnu;
-export SL_CPU=k8;
-export SL_ARCH=$(echo ${SL_TARGET} | sed -e 's/-.*//' -e 's/i.86/i386/');
-export SL_ENDIAN=little;
-export CC="${SL_TARGET}-gcc";
-export CXX="${SL_TARGET}-g++";
-export CPP="${SL_TARGET}-gcc -E";
-export AR="${SL_TARGET}-ar";
-export AS="${SL_TARGET}-as";
-export LD="${SL_TARGET}-ld";
-export RANLIB="${SL_TARGET}-ranlib";
-export READELF="${SL_TARGET}-readelf";
-export STRIP="${SL_TARGET}-strip";
-
-
 #unneccessary?? cd ${SL}/;
-
 
 echo "===================================================================";
 echo "uncompressing busybox tarball...";
