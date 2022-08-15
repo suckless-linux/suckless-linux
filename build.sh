b663e76f7121c59dabd315a7a0800f48a30a75d8
Lucy Mielke <mielkesteven@icloud.com>
2022-08-15 18:45:43 +0200

    changed

--- build.sh
+++ build.sh
@@ -9,10 +9,22 @@ set -e
 set -x
 
 ## mount 
-export LFS=/mnt/LFS
+export LFS=/mnt/lfs
 
-# If you want to run that, please change sdb1 to your desired drive
+## If you want to run that, please change sdb1 to your desired drive
 mkdir -pv $LFS
-mount -v -t ext4 /dev/sdb1 $LFS 
+mount -v -t ext4 /dev/sdb1 $LFS
+
+
+## download all neccessary tarballs using the wget-list
+# source: 
+# https://www.linuxfromscratch.org/lfs/view/stable-systemd/wget-list
+
+wget --input-file=wget-list --continue --directory-prefix=$(pwd)/sources
+
+## check md5 sums
+pushd sources/
+md5sum -c md5sums
+popd
 
 
