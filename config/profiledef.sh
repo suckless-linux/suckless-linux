b405d544584ea76e7a10e48b928919871a02f2ee
Lucy Mielke <mielkesteven@icloud.com>
2022-08-27 12:44:12 +0200

    add packages, change profiledef

--- config/profiledef.sh
+++ config/profiledef.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="suckless"
 iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
-iso_version="$(date +%Y.%m.%d)"
-install_dir="arch"
+iso_publisher="Lucy Mielke <mielkesteven@icloud.com>"
+iso_application="Suckless Linux live environment"
+iso_version="1.0.0_$(date +%Y.%m.%d)"
+install_dir="~/arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
